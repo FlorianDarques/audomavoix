8f2724f9472cccc4adfb64504bc2980fee2a5c26
LucasBertaud <l.bertaud@hotmail.com>
2022-12-09 07:38:47 +0000

    v68

--- connexion.php
+++ connexion.php
@@ -1,7 +1,9 @@
 <?php
 session_start();
-// error_reporting(E-ALL);
-// ini_set("display_errors",1);
+if (isset($_SESSION["user"])) {
+    header("Location: member.php");
+    exit;
+}
 
 $email = $_POST["email"];
 $pass = $_POST["pass"];
@@ -16,48 +18,33 @@ if (!empty($_POST)) {
             
             require "includes/connect.php";
 
-            $sql = "SELECT * FROM `member` WHERE `email` = :email";
+            $sql = "SELECT * FROM `Member` WHERE `email` = :email";
             $query = $db->prepare($sql);
             $query->bindValue(":email", $email, PDO::PARAM_STR);
             $query->execute();
             $user = $query->fetch();
 
             if (!$user) {
-                
-                // header("Location: connexion.php");
                 $_SESSION["error"][] = "Utilisateur ou mot de passe incorrect";
-
             } else if (!password_verify($_POST["pass"], $user["pass"])) {
-                
-                // header("Location: connexion.php");
                 $_SESSION["error"][] = "Utilisateur ou mot de passe incorrect";
-                
             }
             if ($_SESSION["error"] === [] && $user["admin"] != NULL) {
-                
-                header("Location: admin/index.php");
                 $_SESSION["admin"] = true;
-            
-            } else if ($_SESSION["error"] === [] && $user["admin"] === NULL){
-                
+                header("Location: admin/index.php");
+            } if ($_SESSION["error"] === [] && $user["admin"] === NULL){
                 $_SESSION["user"] = [
-                    "id" => $user['ID'],
-                    "lastname" => $user['lastname'],
-                    "firstname" => $user['firstname'],
-                    "age" => $user['age'],
-                    "email" => $user['email']
+                    "id" => $id,
+                    "lastname" => $lastname,
+                    "firstname" => $firstname,
+                    "age" => $age,
+                    "email" => $email
                 ];
-
-
-
-                header('Location: member.php');
-                
-                    
+                header("Location: member.php");
             } 
         }
     }
 }
-
 ?>
 
 <?php 
